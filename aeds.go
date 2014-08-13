1ab16a4586adc218f4cb057e2c89cf3592ce52f3
Michael Hendricks <michael@ndrix.org>
2014-08-13 11:59:27 -0600

    Optionally cache entities in memcache
    Types that implement aeds.Entity may now choose whether or not they are
    cached in memcache.  If entities are eligible for caching, this can
    vastly improve lookup performance.
    
    Upgrading to this version of aeds requires that each Entity implement
    the new CacheTtl() method.

--- aeds.go
+++ aeds.go
@@ -1,15 +1,27 @@
 package aeds
 
 import (
+	"bytes"
+	"encoding/gob"
+	"time"
+
 	"appengine"
 	"appengine/datastore"
+	"appengine/memcache"
 )
 
 // interface for structures that can be stored in App Engine's datastore
 type Entity interface {
 	Kind() string
 	StringId() string
+
 	HookBeforePut() // Calculate derived fields before writing to datastore
+
+	// CacheTtl indicates how long the entity should be cached in memcache.
+	// Return zero to disable memcache.  If this method returns a non-zero
+	// duration, the receiver should also implement the GobEncoder and
+	// GobDecoder interfaces.
+	CacheTtl() time.Duration
 }
 
 // Key returns a datastore key for this entity.
@@ -19,23 +31,77 @@ func Key(c appengine.Context, e Entity) *datastore.Key {
 
 // Put stores an entity in the datastore.
 func Put(c appengine.Context, e Entity) (*datastore.Key, error) {
-
-	// store the event itself
 	e.HookBeforePut()
-	return datastore.Put(c, Key(c, e), e)
+	ttl := e.CacheTtl()
+
+	// encode entity as a gob (before storing in datastore)
+	var value bytes.Buffer
+	if ttl > 0 {
+		err := gob.NewEncoder(&value).Encode(e)
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	// store entity in the datastore
+	lookupKey := Key(c, e)
+	key, err := datastore.Put(c, lookupKey, e)
+	if err != nil {
+		return nil, err
+	}
+
+	// store entity in memcache too?
+	if ttl > 0 {
+		item := &memcache.Item{
+			Key:        lookupKey.String(),
+			Value:      value.Bytes(),
+			Expiration: ttl,
+		}
+		err := memcache.Set(c, item)
+		_ = err // ignore memcache errors
+	}
+
+	return key, nil
 }
 
 // Delete removes an entity from the datastore.
 func Delete(c appengine.Context, e Entity) error {
-	return datastore.Delete(c, Key(c, e))
+	lookupKey := Key(c, e)
+
+	// should the entity be removed from memcache too?
+	if e.CacheTtl() > 0 {
+		err := memcache.Delete(c, lookupKey.String())
+		if err == memcache.ErrCacheMiss {
+			// noop
+		} else if err != nil {
+			return err
+		}
+	}
+
+	return datastore.Delete(c, lookupKey)
 }
 
 // FromId fetches an entity based on its ID.  The given entity
 // should have enough data to calculate the entity's key.  On
 // success, the entity is modified in place with all data from
 // the datastore.
+// Field mismatch errors are ignored.
 func FromId(c appengine.Context, e Entity) (Entity, error) {
-	err := datastore.Get(c, Key(c, e), e)
+	lookupKey := Key(c, e)
+
+	// should we look in memcache too?
+	if e.CacheTtl() > 0 {
+		item, err := memcache.Get(c, lookupKey.String())
+		if err == nil {
+			buf := bytes.NewBuffer(item.Value)
+			err := gob.NewDecoder(buf).Decode(e)
+			return e, err
+		}
+		// ignore any memcache errors
+	}
+
+	// look in the datastore
+	err := datastore.Get(c, lookupKey, e)
 	if err == nil {
 		return e, nil
 	}
